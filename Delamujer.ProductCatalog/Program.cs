4db6fa6de5dcddf6ba61f0a35d271752a861e8ee
Juan Pablo Garcia Blanco <juan1016gaba@gmail.com>
2026-09-08 22:55:56 -0500

    Añade proyectos Application, Domain e Infrastructure
    Se agregan los proyectos Application, Domain e Infrastructure con sus archivos .csproj y clases base. Se establecen referencias entre ellos: Application a Domain e Infrastructure a Application. Se actualiza Delamujer.ProductCatalog.csproj para incluir Swashbuckle.AspNetCore y Application. Se configura Swagger/OpenAPI en Program.cs y se ajusta launchSettings.json para abrir automáticamente la ruta "swagger".

--- Delamujer.ProductCatalog/Program.cs
+++ Delamujer.ProductCatalog/Program.cs
@@ -2,8 +2,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -14,6 +18,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
